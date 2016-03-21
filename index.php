5a0d472309d4d84c3ce6906a3b74452675d75128
Alumno <Alumno>
2016-03-21 16:57:09 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>microsf</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<marquee>
+<h1>bienvenido al mundo heroku soy GUILLERMO</h1>
+</marquee>
+<center>
+<img src="1.jpg" width="1500" alt="800">
+</center>
+<<h2> PROFESOR APRUEVEME CON 13</h2>
+<a target="_blank" href="https://www.google.com.pe">IR A GOOGLE</a>	
+</body>
+</html>
